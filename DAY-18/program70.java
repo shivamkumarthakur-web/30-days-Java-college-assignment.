749e74741163abc84a16d17222130fd1ec495e33
shivamkumarthakur-web <shivamkrpbic@gmail.com>
2026-06-26 23:16:38 +0530

    Add Day 18 programs

---
+++ DAY-18/program70.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class program70{//selection sort.
+public static void selectionsort(int arr[]){
+    for(int i=0;i<arr.length-1;i++){
+
+        int minindex=i;
+
+        for(int j=i+1;j<arr.length;j++){
+
+            if(arr[j]<arr[minindex]){
+                minindex=j;
+            }
+
+        }
+
+        int temp=arr[i];
+        arr[i]=arr[minindex];
+        arr[minindex]=temp;
+    }
+
+    for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }
+}
+
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+
+    selectionsort(arr);
+}
+}
---
+++ DAY-18/program71.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class program71{//binary search.
+public static int binarysearch(int arr[],int key){
+    int start=0;
+    int end=arr.length-1;
+
+    while(start<=end){
+
+        int mid=(start+end)/2;
+
+        if(arr[mid]==key){
+            return mid;
+        }
+
+        else if(arr[mid]<key){
+            start=mid+1;
+        }
+
+        else{
+            end=mid-1;
+        }
+
+    }
+
+    return -1;
+}
+
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+
+    int key=sc.nextInt();
+
+    System.out.println(binarysearch(arr,key));
+}
+}
